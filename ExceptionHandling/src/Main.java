c135c8bbcdb7b4bfaaac945dce6ddc55379d567a
Rishabh Kumar <110474658+rishabhkr1@users.noreply.github.com>
2025-01-19 20:57:10 +0530

    Add files via upload

---
+++ ExceptionHandling/src/Main.java
@@ -0,0 +1,45 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+
+
+//            int dividend = 10,divisor = 0;
+//            try {
+//                int result = dividend / divisor; // Division by zero
+//                System.out.println("Result: " + result);
+//            } catch (ArithmeticException e) {
+//                System.out.println("Error: Division by zero is not allowed.");
+//            }
+//            finally {
+//                System.out.println("Execution completed");
+//            }
+
+        //-----------------------------------------------------------------------------------
+//
+//        int[] numbers = {1, 2, 3, 4, 5};
+//        try {
+//            int index = 10;
+//            int value = numbers[index];
+//            System.out.println("Value at index " + index + ": " + value);
+//        } catch (ArrayIndexOutOfBoundsException e) {
+//            System.out.println("Error: Index is out of bounds.");
+//    }
+
+        //---------------------------------------------------------------------------------
+
+//        try{
+//            int a[]=new int[5];
+//            System.out.println(a[10]);
+//
+//        }
+//        catch(ArithmeticException | ArrayIndexOutOfBoundsException | NumberFormatException e){
+//            System.out.println("There is a exception in a program...");
+//
+//            Thread.sleep(5000);
+//        }finally {
+//            System.out.println("Program terminates");
+//        }
+
+    }
+}
